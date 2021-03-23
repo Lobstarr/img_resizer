36c4b55daec44732a5a0355bfbb53a748dd3b6e8
krabs <anton93@gmail.com>
2021-03-23 13:59:48 +0300

    tests for url and paths

--- resizer.py
+++ resizer.py
@@ -3,8 +3,11 @@ from PIL import Image
 from pprint import pprint
 
 rename_flag = True
+resize_flag = False
+tmb_flag = True
+tmb_size = 300
 
-def resize_tmb_img(img_name, img_src_path, img_dst_path, out_size):
+def resize_tmb_img(img_name, img_src_path, img_dst_path, img_dst_name, out_size):
     im = Image.open(os.path.join(img_src_path, img_name))
     out_size = (out_size, out_size)
     im.thumbnail(out_size)
@@ -18,20 +21,20 @@ def resize_tmb_img(img_name, img_src_path, img_dst_path, out_size):
         new_im.paste(im, box=(round((out_size[0] - img_size[0]) / 2), 0))
 
     # new_im.show()
-    new_im.save(os.path.join(img_dst_path, img_name), "JPEG")
+    new_im.save(os.path.join(img_dst_path, img_dst_name), "JPEG")
 
 
-inputpath = 'C:\\Users\\krabs\\Documents\\Site image resize\\original_img'
+inputpath = 'C:\\Users\\krabs\\Desktop\\Matisse\\original_img'
 # inputpath = os.path.join(os.getcwd(), "original_img")
 outputpath = os.path.join(os.getcwd(), "output_img")
 # print(inputpath)
 # pprint(list(os.walk(inputpath)))
 
 for dirpath, dirnames, filenames in os.walk(inputpath):
-    structure = os.path.join(outputpath, os.path.relpath(dirpath, inputpath))
-    #print(os.walk(inputpath))
-    if not os.path.isdir(structure):
-        os.mkdir(structure)
+    out_path = os.path.join(outputpath, os.path.relpath(dirpath, inputpath))
+    print(out_path)
+    if not os.path.isdir(out_path):
+        os.mkdir(out_path)
         print('Processing ' + dirpath)
     else:
         #print(dirpath + " Folder does already exits!")
@@ -39,14 +42,29 @@ for dirpath, dirnames, filenames in os.walk(inputpath):
 
     # print("list ")
     path_list = list(os.path.split(dirpath))
-    print(dirpath)
-    print(list(path_list))
+    #print(dirpath)
+    #print(list(path_list))
 
+    if dirpath == inputpath:
+        True
+        # print("root")
+    else:
+        artikul = os.path.relpath(dirpath, inputpath)
+        resize_tmb_img(filenames[0], dirpath, out_path, artikul + "_tmb" + ".jpg", 1200)
+        #print("artikul: " + os.path.relpath(dirpath, inputpath))
+    True
+
+
+    if tmb_flag:
+        True
+
+    counter = 1
     for file in filenames:
         # print('in dir ' + os.path.relpath(dirpath, inputpath) + " there are file " + file)
         if file.endswith('.jpg'):
+
             if rename_flag:
-                # resize_tmb_img(file, dirpath, structure, 1200)
+                #resize_tmb_img(file, dirpath, structure, 1200)
                 True
             else:
                 #resize_tmb_img(file, dirpath, structure, 1200)
