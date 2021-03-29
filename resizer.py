360e3abd02000a83f4d63fe59864c2019dcccd41
krabs <anton93@gmail.com>
2021-03-29 14:00:39 +0300

    Added input path creation

--- resizer.py
+++ resizer.py
@@ -1,15 +1,11 @@
 import os
 from PIL import Image
 from shutil import copyfile
+import configparser
 
-rename_flag = True
-resize_flag = False
-resize_size = 1280
-tmb_flag = True
-tmb_size = 300
-url_flag = False
 filenames_arr = []
-remote_filepath = ""
+
+
 
 def prepare_for_url(text):
     text = text.lower()
@@ -36,6 +32,72 @@ def resize_tmb_img(img_name, img_src_path, img_dst_path, img_dst_name, out_size)
     new_im.save(os.path.join(img_dst_path, img_dst_name), "JPEG")
 
 
+def do_the_job():
+    for dirpath, dirnames, filenames in os.walk(inputpath):
+
+        if dirpath == inputpath:
+            # nothing to do in root
+            pass
+            # print("root")
+        elif "\\" in os.path.relpath(dirpath, inputpath):
+            # if subdir
+            print("skipping subdir " + str(os.path.relpath(dirpath, inputpath)))
+        else:
+            # if not subdir
+            if url_flag:
+                artikul = prepare_for_url(os.path.relpath(dirpath, inputpath))
+            else:
+                artikul = os.path.relpath(dirpath, inputpath)
+
+            out_path = os.path.join(outputpath, artikul)
+
+            if not os.path.isdir(out_path):
+                os.mkdir(out_path)
+                print('Processing ' + dirpath)
+            else:
+                print(dirpath + " Folder does already exits!")
+
+            # remove everything except jpg
+            for file in filenames:
+                if not file.endswith('.jpg'):
+                    filenames.remove(file)
+
+            # generate thumbnails
+            if tmb_flag:
+                resize_tmb_img(filenames[0], dirpath, out_path, artikul + "_tmb" + ".jpg", tmb_size)
+            # print("artikul: " + os.path.relpath(dirpath, inputpath))
+
+            counter = 0
+            out_filenames = []
+            for file in filenames:
+                # print('in dir ' + os.path.relpath(dirpath, inputpath) + " there are file " + file)
+
+                counter += 1
+
+                if rename_flag:
+                    out_filename = artikul + "_" + str(counter) + ".jpg"
+                else:
+                    out_filename = file
+
+                if url_flag:
+                    out_filename = prepare_for_url(out_filename)
+
+                out_filenames.append([out_filename, artikul])
+                if resize_flag:
+                    resize_tmb_img(file, dirpath, out_path, out_filename, resize_size)
+                else:
+                    # print("copying" + os.path.join(dirpath, file) + " to " + os.path.join(out_path, out_filename))
+                    copyfile(os.path.join(dirpath, file), os.path.join(out_path, out_filename))
+            filenames_arr.append(out_filenames)
+
+    f = open("filenames.txt", "w+")
+    for art in filenames_arr:
+        out_str = ""
+        for file in art:
+            out_str += remote_filepath + file[1] + "/" + file[0] + ";"
+        f.write(out_str + "\n")
+    f.close()
+
 # inputpath = 'C:\\Users\\krabs\\Desktop\\Matisse\\original_img'
 inputpath = os.path.join(os.getcwd(), "original_img")
 outputpath = os.path.join(os.getcwd(), "output_img")
@@ -46,79 +108,17 @@ if not os.path.isdir(inputpath):
 if not os.path.isdir(outputpath):
     os.mkdir(outputpath)
 
-for dirpath, dirnames, filenames in os.walk(inputpath):
+config = configparser.ConfigParser()  # создаём объекта парсера
+config.read("resizer_config.ini")  # читаем конфиг
 
-    if dirpath == inputpath:
-        # nothing to do in root
-        True
-        # print("root")
-    elif "\\" in os.path.relpath(dirpath, inputpath):
-        # if subdir
-        print("skipping subdir " + str(os.path.relpath(dirpath, inputpath)))
-    else:
-        # if not subdir
-        if url_flag:
-            artikul = prepare_for_url(os.path.relpath(dirpath, inputpath))
-        else:
-            artikul = os.path.relpath(dirpath, inputpath)
+rename_flag = config["global_settings"].getboolean("rename_flag")
+resize_flag = config["global_settings"].getboolean("resize_flag")
+resize_size = config["global_settings"].getint("resize_size")
+tmb_flag = config["global_settings"].getboolean("tmb_flag")
+tmb_size = config["global_settings"].getint("tmb_size")
+url_flag = config["global_settings"].getboolean("url_flag")
+remote_filepath = config["paths"]["loftit_path"]
 
-        out_path = os.path.join(outputpath, artikul)
+do_the_job()
 
-        if not os.path.isdir(out_path):
-            os.mkdir(out_path)
-            print('Processing ' + dirpath)
-        else:
-            print(dirpath + " Folder does already exits!")
-
-        # remove everything except jpg
-        for file in filenames:
-            if not file.endswith('.jpg'):
-                filenames.remove(file)
-
-        # generate thumbnails
-        if tmb_flag:
-            resize_tmb_img(filenames[0], dirpath, out_path, artikul + "_tmb" + ".jpg", tmb_size)
-        # print("artikul: " + os.path.relpath(dirpath, inputpath))
-
-        counter = 0
-        out_filenames = []
-        for file in filenames:
-            # print('in dir ' + os.path.relpath(dirpath, inputpath) + " there are file " + file)
-
-
-
-            counter += 1
-
-            if rename_flag:
-                out_filename = artikul + "_" + str(counter) + ".jpg"
-            else:
-                out_filename = file
-
-            if url_flag:
-                out_filename = prepare_for_url(out_filename)
-
-            out_filenames.append([out_filename, artikul])
-            if resize_flag:
-                resize_tmb_img(file, dirpath, out_path, out_filename, resize_size)
-            else:
-                # print("copying" + os.path.join(dirpath, file) + " to " + os.path.join(out_path, out_filename))
-                copyfile(os.path.join(dirpath, file), os.path.join(out_path, out_filename))
-        filenames_arr.append(out_filenames)
-
-try:
-    path_file = open("path.txt", "r")
-    lines = path_file.readlines()
-    remote_filepath = lines[0].strip()
-except IOError:
-    print("Error: Couldn't open file!.")
-    remote_filepath = ""
-
-
-f = open("filenames.txt", "w+")
-for art in filenames_arr:
-    out_str = ""
-    for file in art:
-        out_str += remote_filepath + file[1] + "/" + file[0] + ";"
-    f.write(out_str + "\n")
-f.close()
 print('DONE!')
