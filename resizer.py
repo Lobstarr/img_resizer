68a925917c406faa6edee22444292435d3262e0f
krabs <anton93@gmail.com>
2021-12-24 12:53:24 +0300

    Minor fixes

--- resizer.py
+++ resizer.py
@@ -5,7 +5,7 @@ import configparser
 
 # pyinstaller --onefile resizer.py --distpath resizer
 
-filenames_arr = []
+
 
 
 def prepare_for_url(text):
@@ -16,7 +16,7 @@ def prepare_for_url(text):
 
 
 def resize_tmb_img(img_name, img_src_path, img_dst_path, img_dst_name, out_size):
-    im = Image.open(os.path.join(img_src_path, img_name))
+    im = Image.open(os.path.join(img_src_path, img_name)).convert('RGB')
     out_size = (out_size, out_size)
     im.thumbnail(out_size)
     img_size = im.size
@@ -29,7 +29,8 @@ def resize_tmb_img(img_name, img_src_path, img_dst_path, img_dst_name, out_size)
         new_im.paste(im, box=(round((out_size[0] - img_size[0]) / 2), 0))
 
     # new_im.show()
-    new_im.save(os.path.join(img_dst_path, img_dst_name), "JPEG")
+    # new_im.save(os.path.join(img_dst_path, img_dst_name), "JPEG")
+    new_im.save(os.path.join(img_dst_path, img_dst_name), "JPEG", subsampling=0)
 
 
 def do_the_job():
@@ -101,29 +102,31 @@ def do_the_job():
         if os.path.exists("filenames.txt"):
             os.remove("filenames.txt")
 
+if __name__ == '__main__':
 
-# inputpath = 'C:\\Users\\krabs\\Desktop\\Matisse\\original_img'
-inputpath = os.path.join(os.getcwd(), "original_img")
-outputpath = os.path.join(os.getcwd(), "output_img")
+    filenames_arr = []
+    # inputpath = 'C:\\Users\\krabs\\Desktop\\Matisse\\original_img'
+    inputpath = os.path.join(os.getcwd(), "original_img")
+    outputpath = os.path.join(os.getcwd(), "output_img")
 
-if not os.path.isdir(inputpath):
-    os.mkdir(inputpath)
+    if not os.path.isdir(inputpath):
+        os.mkdir(inputpath)
 
-if not os.path.isdir(outputpath):
-    os.mkdir(outputpath)
-config = configparser.ConfigParser()  # создаём объекта парсера
-config.read("resizer_config.ini")  # читаем конфиг
+    if not os.path.isdir(outputpath):
+        os.mkdir(outputpath)
+    config = configparser.ConfigParser()  # создаём объекта парсера
+    config.read("resizer_config.ini")  # читаем конфиг
 
-rename_flag = config["global_settings"].getboolean("rename_flag")
-resize_flag = config["global_settings"].getboolean("resize_flag")
-resize_size = config["global_settings"].getint("resize_size")
-tmb_flag = config["global_settings"].getboolean("tmb_flag")
-tmb_size = config["global_settings"].getint("tmb_size")
-url_flag = config["global_settings"].getboolean("url_flag")
-remote_filepath = config["paths"]["loftit_path"]
-url_replace = config["global_settings"]["url_replace"]
-url_replace = url_replace.split(",")
+    rename_flag = config["global_settings"].getboolean("rename_flag")
+    resize_flag = config["global_settings"].getboolean("resize_flag")
+    resize_size = config["global_settings"].getint("resize_size")
+    tmb_flag = config["global_settings"].getboolean("tmb_flag")
+    tmb_size = config["global_settings"].getint("tmb_size")
+    url_flag = config["global_settings"].getboolean("url_flag")
+    remote_filepath = config["paths"]["loftit_path"]
+    url_replace = config["global_settings"]["url_replace"]
+    url_replace = url_replace.split(",")
 
-do_the_job()
+    do_the_job()
 
-print('DONE!')
+    print('DONE!')
