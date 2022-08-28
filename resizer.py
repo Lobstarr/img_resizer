30b5d4aeccbecdcd5d2fdae16e10c87e0907f48e
krabs <anton93@gmail.com>
2022-08-28 17:29:29 +0300

    Minor fixes

--- resizer.py
+++ resizer.py
@@ -1,11 +1,130 @@
 import os
-import PIL
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 from shutil import copyfile
 import configparser
+from openpyxl import Workbook
+from openpyxl.worksheet.dimensions import DimensionHolder, ColumnDimension
 
 
 # pyinstaller --onefile resizer.py --distpath resizer
+
+class ImagesFolder:
+    convert_filenames_to_url = False
+    rename_files_as_dir = False
+    regenerate_files = True
+    resize_size = (1200, 1200)
+    print_current_file = True
+
+    resize_images = False
+    center_when_upscaling = True
+
+    tmb_flag = False
+    tmb_size = (300, 300)
+
+    script_root_path = os.getcwd()
+    src_root_path = os.path.join(script_root_path, 'input_dir')
+    dst_root_path = os.path.join(script_root_path, 'output_dir')
+
+    link_path = ''
+
+    def __init__(self, folder_path, files_inside):
+
+        self.src_folder_name = os.path.relpath(folder_path, inputpath)
+        self.src_folder_name_url_converted = convert_to_url(self.src_folder_name)
+        self.dst_folder_name = \
+            self.src_folder_name_url_converted if self.convert_filenames_to_url else self.src_folder_name
+
+        self.src_folder_path = folder_path
+        self.dst_folder_path = os.path.join(self.dst_root_path, self.dst_folder_name)
+
+        self.src_photo_names_arr = [file for file in files_inside if file.lower().endswith('.jpg')]
+        self.src_photo_paths_arr = [os.path.join(self.src_folder_path, file) for file in self.src_photo_names_arr]
+
+        self.dst_photo_names_arr = []
+        self.fill_dst_photo_names()
+
+        self.dst_photo_paths_arr = \
+            [os.path.join(self.dst_folder_path, filename) for filename in self.dst_photo_names_arr]
+        self.dst_photo_links_arr = \
+            [self.link_path + self.dst_folder_name + "/" + file for file in self.dst_photo_names_arr]
+
+        self.errors_arr = []
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+    def generate_output_folder(self):
+        if not os.path.isdir(self.dst_folder_path):
+            os.mkdir(self.dst_folder_path)
+            print('Processing ' + self.dst_folder_path)
+            return True
+        elif self.regenerate_files:
+            for item in os.listdir(self.dst_folder_path):
+                os.remove(os.path.join(self.dst_folder_path, item))
+            return True
+        else:
+            print(self.src_folder_path + " Output folder already exits!")
+            return False
+
+    def fill_dst_photo_names(self):
+        dst_photo_names_arr = []
+        for photo_number in range(len(self.src_photo_names_arr)):
+            if self.rename_files_as_dir:
+                new_filename = self.src_folder_name + "_" + str(photo_number).zfill(3) + ".jpg"
+            else:
+                new_filename = self.src_photo_names_arr[photo_number]
+            if self.convert_filenames_to_url:
+                new_filename = convert_to_url(new_filename)
+
+            dst_photo_names_arr.append(new_filename)
+        self.dst_photo_names_arr = dst_photo_names_arr
+
+    def generate_output_files(self):
+        for photo_number in range(len(self.src_photo_names_arr)):
+            if self.resize_images:
+                if not resize_tmb_img(
+                        self.src_photo_paths_arr[photo_number],
+                        self.dst_photo_paths_arr[photo_number],
+                        self.resize_size,
+                        center=self.center_when_upscaling):
+                    self.errors_arr.append(self.src_photo_paths_arr[photo_number])
+            else:
+                copyfile(self.src_photo_paths_arr[photo_number], self.dst_photo_paths_arr[photo_number])
+
+            if self.print_current_file:
+                print('File', self.src_photo_paths_arr[photo_number], 'processed.')
+        if self.tmb_flag:
+            self.generate_tmb()
+
+    def generate_tmb(self):
+        if self.src_photo_paths_arr[0]:
+            if self.rename_files_as_dir:
+                tmb_dst_filename = self.dst_folder_name + '_tmb.jpg'
+            else:
+                tmb_dst_filename = os.path.splitext(self.dst_photo_names_arr[0])[0]
+                tmb_dst_filename = tmb_dst_filename + '_tmb.jpg'
+
+            dst_tmb_filepath = os.path.join(self.dst_folder_path, tmb_dst_filename)
+
+            if not resize_tmb_img(
+                    self.src_photo_paths_arr[0],
+                    dst_tmb_filepath,
+                    self.tmb_size,
+                    center=self.center_when_upscaling):
+                self.errors_arr.append('Error while generating tmb from ' + str(self.src_photo_paths_arr[0]))
+            else:
+                self.dst_photo_names_arr.append(tmb_dst_filename)
+                self.src_photo_paths_arr.append(dst_tmb_filepath)
+                if self.print_current_file:
+                    print('Tmb for', self.src_photo_paths_arr[0], 'created.')
+        else:
+            self.errors_arr.append('No src file for tmb in ' + str(self.src_folder_path))
+
+    def get_process_report(self):
+        return [self.src_folder_name, self.dst_folder_name, *self.dst_photo_links_arr]
 
 
 def gen_config():
@@ -30,21 +149,21 @@ def gen_config():
         f.write(conf_str)
 
 
-def prepare_for_url(text):
+def convert_to_url(text):
     text = text.lower()
     for symbol in url_replace:
         text = text.replace(symbol, "-")
     return text
 
 
-def resize_tmb_img(img_name, img_src_path, img_dst_path, img_dst_name, out_size, center=True):
+def resize_tmb_img(img_src_path, img_dst_path, out_size, center=True):
     try:
-        im = Image.open(os.path.join(img_src_path, img_name)).convert('RGB')
-    except PIL.UnidentifiedImageError:
-        print('Format of', img_name, 'at', img_src_path, 'is not jpg!')
+        im = Image.open(img_src_path).convert('RGB')
+    except UnidentifiedImageError:
+        print('Format of', img_src_path, 'is not jpg!')
         return False
     except:
-        print('Something wrong with', img_name, 'at', img_src_path)
+        print('Something wrong with', img_src_path)
         return False
 
     im.thumbnail(out_size)
@@ -64,97 +183,43 @@ def resize_tmb_img(img_name, img_src_path, img_dst_path, img_dst_name, out_size,
 
     new_im.paste(im, box=margins)
     # new_im.show()
-    # new_im.save(os.path.join(img_dst_path, img_dst_name), "JPEG")
-    new_im.save(os.path.join(img_dst_path, img_dst_name), "JPEG", subsampling=0)
+    new_im.save(img_dst_path, "JPEG", subsampling=0)
 
     return True
 
 
 def do_the_job():
     errors_arr = []
-    for dirpath, dirnames, filenames in os.walk(inputpath):
 
-        if dirpath == inputpath:
+    wb = Workbook()
+    ws = wb.active
+    ws.append(['src folder', 'dst folder', 'photos'])
+
+    for current_dir_path, subdir_names, files_inside in os.walk(inputpath):
+
+        if current_dir_path == inputpath:
             # nothing to do in root
             continue
             # print("root")
-        elif "\\" in os.path.relpath(dirpath, inputpath):
+        elif "\\" in os.path.relpath(current_dir_path, inputpath):
             # if subdir
-            print("skipping subdir " + str(os.path.relpath(dirpath, inputpath)))
+            print("skipping subdir " + str(os.path.relpath(current_dir_path, inputpath)))
             continue
 
         # if not subdir
-        if url_flag:
-            artikul = prepare_for_url(os.path.relpath(dirpath, inputpath))
-        else:
-            artikul = os.path.relpath(dirpath, inputpath)
-
-        out_path = os.path.join(outputpath, artikul)
-
-        if not os.path.isdir(out_path):
-            os.mkdir(out_path)
-            print('Processing ' + dirpath)
-        elif regenerate:
-            for item in os.listdir(out_path):
-                os.remove(os.path.join(out_path, item))
-        else:
-            print(dirpath + " Folder does already exits!")
+        this_dir_obj = ImagesFolder(current_dir_path, files_inside)
+        if not this_dir_obj.generate_output_folder():
             continue
+        this_dir_obj.generate_output_files()
 
-        # remove everything except jpg
-        for file in filenames[::-1]:
-            if not file.lower().endswith('.jpg'):
-                filenames.remove(file)
+        ws.append(this_dir_obj.get_process_report())
+        errors_arr = [*errors_arr, *this_dir_obj.errors_arr]
 
-        # generate thumbnails
-        if tmb_flag:
-            if not resize_tmb_img(filenames[0], dirpath, out_path, artikul + "_tmb" + ".jpg", tmb_size):
-                errors_arr.append((dirpath, filenames[0]))
-                continue
-        # print("artikul: " + os.path.relpath(dirpath, inputpath))
+    wb.save('Resizer_output.xlsx')
 
-        counter = 0
-        out_filenames = []
-        for file in filenames:
-            # print('in dir ' + os.path.relpath(dirpath, inputpath) + " there are file " + file)
-
-            counter += 1
-
-            if rename_flag:
-                out_filename = artikul + "_" + str(counter).zfill(3) + ".jpg"
-            else:
-                out_filename = file
-
-            if url_flag:
-                out_filename = prepare_for_url(out_filename)
-
-            out_filenames.append([out_filename, artikul])
-            if resize_flag:
-                if not resize_tmb_img(file, dirpath, out_path, out_filename, resize_size, center=center_flag):
-                    errors_arr.append((dirpath, file))
-                    continue
-            else:
-                # print("copying" + os.path.join(dirpath, file) + " to " + os.path.join(out_path, out_filename))
-                copyfile(os.path.join(dirpath, file), os.path.join(out_path, out_filename))
-
-            if list_files_flag:
-                print('File', file, 'at', dirpath, 'is done.')
-
-        filenames_arr.append(out_filenames)
     if errors_arr:
         print('These files were not processed due to resize error:')
         print(errors_arr)
-
-    if url_flag:
-        with open("filenames.txt", 'w') as f:
-            for art in filenames_arr:
-                out_str = ""
-                for file in art:
-                    out_str += remote_filepath + file[1] + "/" + file[0] + ";"
-                f.write(out_str + "\n")
-    else:
-        if os.path.exists("filenames.txt"):
-            os.remove("filenames.txt")
 
 
 if __name__ == '__main__':
@@ -162,6 +227,9 @@ if __name__ == '__main__':
     filenames_arr = []
     inputpath = os.path.join(os.getcwd(), "original_img")
     outputpath = os.path.join(os.getcwd(), "output_img")
+
+    ImagesFolder.src_root_path = inputpath
+    ImagesFolder.dst_root_path = outputpath
 
     if not os.path.isdir(inputpath):
         os.mkdir(inputpath)
@@ -173,27 +241,27 @@ if __name__ == '__main__':
     config = configparser.ConfigParser()  # создаём объекта парсера
     config.read("resizer_config.ini")  # читаем конфиг
 
-    rename_flag = config["global_settings"].getboolean("rename_flag")
+    ImagesFolder.rename_files_as_dir = config["global_settings"].getboolean("rename_flag")
 
-    url_flag = config["global_settings"].getboolean("url_flag")
+    ImagesFolder.convert_filenames_to_url = config["global_settings"].getboolean("url_flag")
     url_replace = config["global_settings"]["url_replace"]
     url_replace = url_replace.split(",")
-    regenerate = config["global_settings"].getboolean("regenerate")
-    list_files_flag = config["global_settings"].getboolean("list_files_flag")
+    ImagesFolder.regenerate_files = config["global_settings"].getboolean("regenerate")
+    ImagesFolder.print_current_file = config["global_settings"].getboolean("list_files_flag")
 
-    resize_flag = config["img_settings"].getboolean("resize_flag")
-    center_flag = config["img_settings"].getboolean("center_flag")
+    ImagesFolder.resize_images = config["img_settings"].getboolean("resize_flag")
+    ImagesFolder.center_when_upscaling = config["img_settings"].getboolean("center_flag")
     resize_width = config["img_settings"].getint("resize_width")
     resize_height = config["img_settings"].getint("resize_height")
-    resize_size = (resize_width, resize_height)
+    ImagesFolder.resize_size = (resize_width, resize_height)
 
-    tmb_flag = config["img_settings"].getboolean("tmb_flag")
+    ImagesFolder.tmb_flag = config["img_settings"].getboolean("tmb_flag")
     tmb_size = config["img_settings"].getint("tmb_size")
-    tmb_size = (tmb_size, tmb_size)
+    ImagesFolder.tmb_size = (tmb_size, tmb_size)
 
-    remote_filepath = config["paths"]["site_path"]
+    ImagesFolder.link_path = config["paths"]["site_path"]
 
     do_the_job()
 
     print('DONE!')
-    input('Press Enter to exit...')
+    # input('Press Enter to exit...')
